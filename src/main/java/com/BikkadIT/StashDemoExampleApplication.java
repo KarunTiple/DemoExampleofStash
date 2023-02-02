6bad7027c7ce9bdaca3c63febc4eb0d18c22fa1f
karuntiple <tiplekarun@gmail.com>
2023-02-02 11:50:06 +0530

    Project by Karun

---
+++ src/main/java/com/BikkadIT/StashDemoExampleApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StashDemoExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StashDemoExampleApplication.class, args);
+	}
+
+}
